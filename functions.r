1d5f470e2c4bcb45f6e7b5de407e7afe02b141fe
Chiel92 <ctje92@gmail.com>
2014-10-07 13:47:46 +0200

    bugfixes

--- functions.r
+++ functions.r
@@ -1,83 +1,30 @@
 # Compute the mode of given data set
-mode <- function(x) {
+mode = function(x) {
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
 
 # Compute intermediate values of list
-intermediate <- function(l) diff(l) / 2 + head(l, -1)
+intermediate = function(l) diff(l) / 2 + head(l, -1)
 
 # pi
 # Exploit the fact that y only contains 1's and 0's
-p <- function(y) sum(y) / length(y)
+p = function(y) sum(y) / length(y)
 
 # Util
-assert <- function(bool) if (!bool) stop('Assertion error.')
+assert = function(bool) if (!bool) stop('Assertion error.')
 
 # Gini index
 # y is a vector of bits indicating the class label
-# Returns 0 if y is empty
-impurity <- function (y) p(y) * (1 - p(y))
-#{
-    #assert(length(y) > 0)
-    #p_1 <- p(y)
-    #assert(p_1 <= 1)
-    #return(p_1 * (1 - p_1))
-#}
-
-# Bestsplit
-# x is a vector of numeric attribute
-# y is a vector of bits indicating the class label
-# Computes the best split w.r.t. the numeric attribute x
-# Doesn't use segment borders
-bestsplit <- function (x, y, minleaf)
-{
-    assert(length(x) == length(y))
-    assert(length(x) > 1)
-
-    # Construct all candidate splits values
-    attribute_values <- sort(unique(x))
-    if (length(attribute_values) <= 1)
-        return(NULL)
-    splitvalues <- intermediate(attribute_values)
-
-    allowed_splits = list()
-    for (splitvalue in splitvalues)
-        if (length(y[x < splitvalue]) >= minleaf & length(y[x >= splitvalue]) >= minleaf)
-            allowed_splits <- c(allowed_splits, splitvalue)
-
-    if (length(allowed_splits) == 0)
-        return(NULL)
-
-    # Assess the potential impurity of given split
-    impurity_reduction <- function(splitvalue)
-    {
-        y1 <- y[x < splitvalue]
-        y2 <- y[x >= splitvalue]
-        return(impurity(y) - (p(y1) * impurity(y1) + p(y2) * impurity(y2)))
-    }
-
-    # Assess the split values and pick the purest
-    # which.min only returns 1 index
-    reductions <- lapply(allowed_splits, impurity_reduction)
-    purest <- which.max(reductions)
-
-    splitvalue <- allowed_splits[[purest]]
-    reduction <- reductions[[purest]]
-
-    return(list(splitvalue = splitvalue, reduction = reduction))
-}
+impurity = function (y) p(y) * (1 - p(y))
 
 # Constructor for tree
 tree = function(rows, classlabels)
 {
-    this = new.env(parent = globalenv())  
+    this = new.env(parent = globalenv())
     class(this) <- "tree"
     this$rows <- rows
     this$classlabels <- classlabels
-    this$splitvalue <- NULL
-    this$leftchild <- NULL
-    this$rightchild <- NULL
     return(this)
 }
 
@@ -92,7 +39,79 @@ set_values = function(object, leftchild, rightchild, splitattribute, splitvalue)
     return(NULL)
 }
 
-tree.grow <- function(x, y, nmin, minleaf)
+# Bestsplitvalue
+# x is a vector of numeric attribute
+# y is a vector of bits indicating the class label
+# Computes the best split value w.r.t. the numeric attribute x
+# Doesn't use segment borders
+bestsplitvalue = function (x, y, minleaf)
+{
+    assert(length(x) == length(y))
+    assert(length(x) > 1)
+
+    # Construct all candidate splits values
+    attribute_values <- sort(unique(x))
+    if (length(attribute_values) <= 1)
+        return(NULL)
+    splitvalues <- intermediate(attribute_values)
+
+    # Make sure only to consider splits which meet the minleaf constraint
+    allowed_splits = list()
+    for (splitvalue in splitvalues)
+        if (length(y[x < splitvalue]) >= minleaf & length(y[x >= splitvalue]) >= minleaf)
+            allowed_splits <- c(allowed_splits, splitvalue)
+
+    if (length(allowed_splits) == 0)
+        return(NULL)
+
+    # Function for computing impurity reduction
+    impurity_reduction <- function(splitvalue)
+    {
+        y1 <- y[x < splitvalue]
+        y2 <- y[x >= splitvalue]
+        return(impurity(y) - (p(y1) * impurity(y1) + p(y2) * impurity(y2)))
+    }
+
+    # Assess split values by impurity reduction and pick the best
+    # which.min only returns 1 index
+    reductions <- lapply(allowed_splits, impurity_reduction)
+    best <- which.max(reductions)
+
+    splitvalue <- allowed_splits[[best]]
+    reduction <- reductions[[best]]
+
+    return(list(splitvalue = splitvalue, reduction = reduction))
+}
+
+# Bestsplit
+# x is a matrix of numeric attributes
+# y is a vector of bits indicating the class label
+# Computes the best split w.r.t. the numeric attributes x
+bestsplit = function (x, y, minleaf)
+{
+    max_reduction <- 0
+    splitvalue <- NULL
+    splitattribute <- NULL
+    for (attribute in 1:NCOL(x))
+    {
+        result <- bestsplitvalue(x[,attribute], y, minleaf)
+        if (is.null(result)) next
+
+        print(result$reduction)
+        if (result$reduction > max_reduction)
+        {
+            max_reduction <- result$reduction
+            splitvalue <- result$splitvalue
+            splitattribute <- attribute
+        }
+    }
+
+    if (is.null(splitattribute))
+        return(NULL)
+    return(list(splitattribute = splitattribute, splitvalue = splitvalue))
+}
+
+tree.grow = function(x, y, nmin, minleaf)
 {
     root <- tree(x, y)
     nodelist <- list(root)
@@ -107,39 +126,21 @@ tree.grow <- function(x, y, nmin, minleaf)
             assert(length(node$classlabels) > 1)
 
             # Compute the best split attribute-value pair
-            max_reduction <- 0
-            splitvalue = NULL
-            splitattribute <- NULL
-            for (attribute in dim(x)[2])
-            {
-                result <- bestsplit(node$rows[, attribute], node$classlabels, minleaf)
-                if (is.null(result)) next
-
-                if (result$reduction > max_reduction)
-                {
-                    max_reduction <- result$reduction
-                    splitvalue <- result$splitvalue
-                    splitattribute <- attribute
-                }
-            }
-
-            if (is.null(splitvalue)) next
+            result <- bestsplit(node$rows, node$classlabels, minleaf)
+            if (is.null(result)) next
+            splitvalue <- result$splitvalue
+            splitattribute <- result$splitattribute
 
             #print(splitvalue)
             #print(splitattribute)
             #print(node$rows[splitattribute])
 
             # Construct the tree children resulting from the split
-            left <- node$rows[, splitattribute] < splitvalue
-            right <- node$rows[, splitattribute] >= splitvalue
+            leftpart <- node$rows[, splitattribute] < splitvalue
+            rightpart <- node$rows[, splitattribute] >= splitvalue
 
-            leftchildrows <- node$rows[left,]
-            rightchildrows <- node$rows[right,]
-            leftchildclasslabels <- node$classlabels[left]
-            rightchildclasslabels <- node$classlabels[right]
-
-            leftchild <- tree(leftchildrows, leftchildclasslabels)
-            rightchild <- tree(rightchildrows, rightchildclasslabels)
+            leftchild <- tree(node$rows[leftpart,], node$classlabels[leftpart])
+            rightchild <- tree(node$rows[rightpart,], node$classlabels[rightpart])
 
             assert(length(leftchild$classlabels) > 0)
             assert(length(rightchild$classlabels) > 0)
@@ -154,22 +155,47 @@ tree.grow <- function(x, y, nmin, minleaf)
     return(root)
 }
 
-tree.classify <- function(x, tr)
+tree.print = function(tr)
 {
-    y <- c()
+    nodelist <- list(tr)
+    while(length(nodelist) > 0)
+    {
+        node <- nodelist[[1]]
+        nodelist[[1]] <- NULL
+
+        # If we are at a leaf
+        if (is.null(node$leftchild))
+        {
+            print('Leaf')
+            next
+        }
+
+        print(paste('splitattribute:', toString(node$splitattribute)))
+        print(paste('splitvalue:', toString(node$splitvalue)))
+
+        nodelist <- c(nodelist, list(node$leftchild, node$rightchild))
+    }
+}
+
+tree.classify = function(x, tr)
+{
+    y <- NULL
     for (row in 1:NROW(x))
     {
         #print(row)
         node <- tr
         # While the current node still has children
+        #print(x[row, node$splitattribute])
         while(!is.null(node$leftchild))
         {
             #print(0)
+            #print(node$splitvalue)
             if (x[row, node$splitattribute] < node$splitvalue)
                 node <- node$leftchild
             else
                 node <- node$rightchild
         }
+        #return(node)
         #print(y)
         #print(node$classlabels)
         y[row] <- mode(node$classlabels)
